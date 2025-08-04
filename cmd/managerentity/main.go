65c258b3259bd330ec5acf8022d5a3d08df78bc6
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 21:37:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/managerentity/main.go
@@ -0,0 +1,23 @@
+// cmd/managerentity/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"managerentity/internal/managerentity"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := managerentity.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
